5604685e20f0ae13d89c387b20dc43ea43248bb6
Brooke <brookepaglia@gmail.com>
2021-12-28 19:59:18 -0500

    add prompts for password length and character types

--- script.js
+++ script.js
@@ -1,42 +1,84 @@
 // Assignment code here
+// Password character type object
+var passTypes = {
+  lowercase: 'abcdefghijklmnopqrstuvwxyz',
+  uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+  numbers: '0123456789',
+  'special characters': '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~',
+};
+
+var passParams = {
+  length: 0,
+  types: [],
+  reset: function () {
+    this.length = 0;
+    this.types = [];
+  },
+};
 
 // Randomly choose a number based on length of passed variable
 var rand = function (howLong) {
   return Math.floor(Math.random() * howLong);
 };
 
-var passLength = function () {
-  var getLength = prompt(
-    'How long should the password be? Password should be between 8 and 128 characters.'
-  );
+// Get password parameters
+var getParams = function () {
+  var count = 0;
+  while (passParams.length === 0 || passParams.types.length === 0) {
+    if (passParams.length === 0) {
+      var getLength = prompt(
+        'How long should the password be? Password should be between 8 and 128 characters.'
+      );
 
-  if (getLength < 8 || getLength > 128) {
-    alert(
-      getLength +
-        ' is an invalid choice. Password should be between 8 and 128 characters.'
-    );
-    passLength();
+      if (getLength < 8 || getLength > 128) {
+        alert(
+          getLength +
+            ' is an invalid choice. Password should be between 8 and 128 characters.'
+        );
+      } else {
+        passParams.length = getLength;
+      }
+    } else if (passParams.types.length === 0) {
+      for (var i in passTypes) {
+        var response = confirm(
+          'Do you want ' + i + ' to be used in your password?'
+        );
+        count++;
+        if (response) {
+          passParams.types.push(i);
+        } else if (
+          !response &&
+          passParams.types.length === 0 &&
+          count == Object.keys(passTypes).length
+        ) {
+          alert('You must choose at least one character type.');
+          getParams();
+        }
+      }
+    }
   }
-  return getLength;
 };
 
 var generatePassword = function () {
   var password = '';
-  var a = 'abcdefghijklmnopqrstuvwxyz';
-  var A = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  var n = '0123456789';
-  var s = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
-  var passwordLength = passLength();
-  var passwordTypes = [a, A, n, s];
+
+  passParams.reset();
+
+  getParams();
+  var passwordLength = passParams.length;
+  var passwordTypes = passParams.types;
 
   // Loop until the password is as long as the user's choice
   while (password.length < passwordLength) {
     // Get a random character type from the user's choices
     var pickedType = passwordTypes[rand(passwordTypes.length)];
 
-    // Get a random character from the picked character type
+    // Get the characters in that type
+    var chars = passTypes[pickedType];
+
+    // Get a random character from the picked characters
     // and add it to the password string
-    password += pickedType[rand(pickedType.length)];
+    password += chars[rand(chars.length)];
   }
   return password;
 };
